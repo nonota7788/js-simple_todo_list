063301092cba26c82cb5b1666eef6d6eb0839e7f
nonota7788 <f.t.fs0924@gmail.com>
2019-08-22 23:30:51 +0900

    implement a feature-delete_todos

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,6 @@
-/**
- * todoは5件まで登録可能
- * todoは上から順に追加されていく
- * todoを個別に削除すると、削除されたtodoより下のtodoは上に詰められる
- */
-
 var todos;
 
+//todoを管理するための配列
 todos = [];
 
 document.getElementById("btn-add").addEventListener("click", function() {
@@ -15,48 +10,57 @@ document.getElementById("btn-add").addEventListener("click", function() {
     addedTodo = document.getElementById("added-todo").value;
     document.getElementById("added-todo").value = "";
 
-    // 1,変数todosに入力された値（todo）を格納する
+    // 1,配列todosに入力された値（todo）を格納する
     todos.push(addedTodo);
+
     // 2,todoリストに表示する
-    var ul = document.querySelector("ul");
+    var ol = document.querySelector("ol");
     var li = document.createElement("li");
-    ul.appendChild(li);
+    ol.appendChild(li);
     li.classList.add("todo-item");
-
     var input = document.createElement("input");
+    var btn_update = document.createElement("button");
+    var btn_delete = document.createElement("button");
     li.appendChild(input);
-
-    //ループで書き直す
-    var createBtnUpdate = document.createElement("button");
-    var createBtnDelete = document.createElement("button");
-    createBtnUpdate.classList.add("btn");
-    createBtnDelete.classList.add("btn");
-    var btn_update = createBtnUpdate;
-    var btn_delete = createBtnDelete;
-    btn_update.classList.add("btn-update");
-    btn_delete.classList.add("btn-delete");
-    btn_update.textContent = "UPDATE";
-    btn_delete.textContent = "DELETE";
-
     li.appendChild(btn_update);
     li.appendChild(btn_delete);
-
-    var allInput = document.querySelectorAll("ul input");
+    btn_update.classList.add("btn", "btn-update");
+    btn_delete.classList.add("btn", "btn-delete");
+    btn_update.textContent = "UPDATE";
+    btn_delete.textContent = "DELETE";
+    /**
+     * 後にtodoを削除・更新する際に、個々のtodoを識別できるよう、
+     * 配列todosのindexと紐づくidを一番上のtodoから０番から順に振る
+     *
+     * liのid属性、更新・削除ボタンのonclick属性の値deleteTodo関数・updateTodo関数の引数、
+     * 配列todosに格納されるtodoのインデックス番号、それぞれの値は、全て同じ値（０〜５）で紐づいている。
+     */
+    var allTodoList = document.querySelectorAll(".todo-item");
     var allBtnDletete = document.querySelectorAll(".btn-delete");
     for (var i = 0; i < todos.length; i++) {
-      allInput[i].classList.add(`todo-${i}`);
-      document.querySelector(`.todo-${i}`).value = todos[i];
+      allTodoList[i].setAttribute("id", i);
+      document.getElementById(i).childNodes[0].value = todos[i];
       allBtnDletete[i].setAttribute("onclick", `deleteTodo(${i})`);
     }
   }
 });
 
-function deleteTodo(todoId) {}
-
-/*function deleteTodo(id) {
-  num = id;
-  todos.splice(id, 1);
-  document.querySelector(`.todo-${id}`).value = "";
-  console.log(todos);
-  imcompList = true;
-}*/
+//delteボタンを押したときに実行される関数
+function deleteTodo(todoId) {
+  //各リストに振られている番号を1つ繰り下げる
+  for (var i = 0; i < todos.length - (todoId + 1); i++) {
+    var updatedList = document.getElementById(`${todoId + (i + 1)}`);
+    var updatedId = updatedList.id;
+    updatedId = updatedId - 1;
+    updatedList.id = updatedId;
+    updatedList.childNodes[2].setAttribute(
+      "onclick",
+      `deleteTodo(${updatedId})`
+    );
+  }
+  //DOMから削除する
+  var deletedList = document.getElementById(todoId);
+  deletedList.parentNode.removeChild(deletedList);
+  //配列todosから削除する
+  todos.splice(todoId, 1);
+}
